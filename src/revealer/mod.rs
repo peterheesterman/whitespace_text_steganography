2942bb03355eace6c3a40158515342f98e152857
Peter Heesterman <heesterman.peter@gmail.com>
2018-10-27 08:43:32 +1300

    test(Revealer): Break apart get_whitespace_groups and test

--- src/revealer/mod.rs
+++ src/revealer/mod.rs
@@ -6,7 +6,7 @@ fn is_zero_width_whitespace_character(candidate: char) -> bool {
     candidate == '​'
 }
 
-pub fn extract(carrier: String) -> String {
+fn get_whitespace_groups(carrier: String) -> Vec<u32> {
     let mut whitespace_groups: Vec<u32> = Vec::new();
 
     let mut carrier = carrier.chars();
@@ -23,11 +23,34 @@ pub fn extract(carrier: String) -> String {
         character = carrier.next().unwrap();
     }
 
+    println!("{:?}", whitespace_groups);
+    whitespace_groups
+}
+
+pub fn extract(carrier: String) -> String {
     let mut payload = String::new();
 
-    for group in whitespace_groups.iter() {
+    for group in get_whitespace_groups(carrier).iter() {
         payload.push(char::from_u32(*group).unwrap());
     }
 
     payload.to_string()
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn can_identify_zero_width_whitespace() {
+        assert!(is_zero_width_whitespace_character('​'))
+    }
+    
+    #[test]
+    fn can_group_white_space() {
+        let whitespace_infested_text = String::from("W​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​h​​​​​​​​​​o​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​so list to hunt");
+        let whitespace_groups_expected: Vec<u32> = vec![49, 10, 50];
+        assert_eq!(get_whitespace_groups(whitespace_infested_text), whitespace_groups_expected);
+    }
 }
